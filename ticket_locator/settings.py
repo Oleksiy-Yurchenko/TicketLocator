d9910ad0b0c219eba12e4a8c1ee1a96deb8d997d
Oleksiy Yurchenko <oleksiy.yurchenko@gmail.com>
2021-04-29 06:24:10 +0300

    Start of the project

--- ticket_locator/settings.py
+++ ticket_locator/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'hello_world',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-AUTH_USER_MODEL = 'users.CustomUser'

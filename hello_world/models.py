19a65016bb351cbb684303b9027f058ba92fdfc6
Oleksiy Yurchenko <oleksiy.yurchenko@gmail.com>
2021-04-29 07:10:29 +0300

    Modified hello_world/admin.py and hello_world/models.py

--- hello_world/models.py
+++ hello_world/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class SearchHistoryPost(models.Model):
 
-    departure_city = models.CharField(max_length=40)
-    arrival_city = models.CharField(max_length=40)
-    departure_date = models.DateField()
-    arrival_date = models.DateField()
+    departure_city = models.CharField(max_length=40, null=False)
+    arrival_city = models.CharField(max_length=40, null=False)
+    departure_date = models.DateField(null=False)
+    arrival_date = models.DateField(null=False)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
